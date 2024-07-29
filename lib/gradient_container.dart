c40995604c0f5e40965156964d90f352e9ba6600
Richard <rdobmk@gmail.com>
2024-07-29 21:07:02 +0200

    12 Styling Buttons

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 
-import 'package:firstapp/style_text.dart';
-
 const startAlignment = Alignment.topLeft;
 const endAlignment = Alignment.bottomRight;
 
@@ -10,6 +8,12 @@ class GradientContainer extends StatelessWidget {
   final Color color2;
 
   const GradientContainer(this.color1, this.color2, {super.key});
+
+  const GradientContainer.purple({super.key})
+      : color1 = Colors.deepPurple,
+        color2 = Colors.indigo;
+
+  void rollDice() {}
 
   @override
   Widget build(context) {
@@ -21,9 +25,22 @@ class GradientContainer extends StatelessWidget {
           end: endAlignment,
         )),
         child: Center(
-            child: Image.asset(
-          'assets/images/dice-2.png',
-          width: 250,
+            child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Image.asset(
+              'assets/images/dice-2.png',
+              width: 250,
+            ),
+            const SizedBox(height: 20),
+            TextButton(
+              onPressed: rollDice,
+              style: TextButton.styleFrom(
+                  foregroundColor: Colors.white,
+                  textStyle: const TextStyle(fontSize: 28)),
+              child: const Text('Roll that Dice'),
+            )
+          ],
         )));
   }
 }
