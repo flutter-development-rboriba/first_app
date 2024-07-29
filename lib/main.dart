5d1edbaf9aed720f75a4e7f9940bdff29ba5471f
Richard <rdobmk@gmail.com>
2024-07-29 20:38:20 +0200

    10 Resusable widgets

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,9 @@ import 'package:firstapp/gradient_container.dart';
 void main() {
   runApp(
     MaterialApp(
-      home: Scaffold(body: GradientContainer()),
+      home: Scaffold(
+          body: GradientContainer(Color.fromARGB(255, 33, 5, 109),
+              Color.fromARGB(255, 68, 21, 149))),
     ),
   );
 }
