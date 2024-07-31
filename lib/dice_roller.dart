470ba207cbedacb8598e28121bd6c03728e66119
Richard <rdobmk@gmail.com>
2024-07-31 20:47:45 +0200

    14 Generating random number

--- lib/dice_roller.dart
+++ lib/dice_roller.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 
+import 'dart:math';
+
+final randomizer = Random();
+
 class DiceRoller extends StatefulWidget {
   const DiceRoller({super.key});
 
@@ -10,11 +14,11 @@ class DiceRoller extends StatefulWidget {
 }
 
 class _DiceRollerState extends State<DiceRoller> {
-  var activeDiceImage = 'assets/images/dice-2.png';
+  var currentDiceRoll = 3;
 
   void rollDice() {
     setState(() {
-      activeDiceImage = 'assets/images/dice-4.png';
+      currentDiceRoll = randomizer.nextInt(6) + 1;
     });
   }
 
@@ -24,7 +28,7 @@ class _DiceRollerState extends State<DiceRoller> {
       mainAxisSize: MainAxisSize.min,
       children: [
         Image.asset(
-          activeDiceImage,
+          'assets/images/dice-$currentDiceRoll.png',
           width: 250,
         ),
         const SizedBox(height: 20),
